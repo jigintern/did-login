3e9b25fc46c200f93fb1a465663abff2a0ff3852
takerucam <m18kecam777@gmail.com>
2023-07-13 10:24:40 +0900

    JSDoc を追加

--- db-controller.js
+++ db-controller.js
@@ -1,7 +1,7 @@
 import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 import "https://deno.land/std@0.192.0/dotenv/load.ts";
 
-// sqlの設定
+// SQLの設定
 const connectionParam = {
   hostname: Deno.env.get("HOST_NAME"),
   username: Deno.env.get("SQL_USER"),
@@ -13,14 +13,28 @@ const connectionParam = {
 // クライアントの作成
 const client = await new Client().connect(connectionParam);
 
-export async function checkIfIdExists(did) {
+/**
+ * 任意のDIDがDBに保存されているかをチェック
+ * DIDがDBに保存されていればtrue、保存されていなければfalseを返す
+ * @param {string} did
+ * @returns {boolean} result
+ */
+export async function checkDIDExists(did) {
   // DBに電子署名があるか
   const res = await client.execute(
     `select count(*) from users where did = "${did}";`
   );
+  // レスポンスのObjectから任意のDIDと保存されているDIDが一致している数を取得し
+  // その数が1かどうかを返す
+  // DBにはDIDが重複されない設計になっているので一致している数は0か1になる
   return res.rows[0][res.fields[0].name] === 1;
 }
 
+/**
+ * 任意のDIDとユーザー名をDBに保存する
+ * @param {string} did
+ * @param {string} userName
+ */
 export async function addDID(did, userName) {
   // DBにデータを追加
   await client.execute(
@@ -28,8 +42,13 @@ export async function addDID(did, userName) {
   );
 }
 
+/**
+ * DBから任意のDIDと一致するレコードを取得
+ * @param {string} did
+ * @returns {Object} result
+ */
 export async function getUser(did) {
-  // DBからsignatureが一致するレコードを取得
+  // DBからDIDが一致するレコードを取得
   const res = await client.execute(`select * from users where did = "${did}";`);
   return res;
 }
